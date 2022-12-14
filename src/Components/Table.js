8c11c152aaee7b85976a78b4b46987aa27100ee2
mohamud <mohmud141@gmail.com>
2022-12-14 11:53:09 +0300

    styling in react

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import './Table.css'
+
 
 function Table() {
     const users = [
-        {name: 'mohamud', age: '35', location: 'addis ababa'},
-        {name: 'lincoln', age: '30', location: 'nairobi'},
-        {name: 'musas', age: '25', location: 'london'}
+        {userName: 'moh144', name: 'mohamud', posts: '10', comments: '11', country: 'ethiopia'},
+        {userName: 'lincon111', name: 'lincoln', posts: '13', comments: '8', country: 'kenya'},
+        {userName: 'musas43',  name: 'musas', posts: '12', comments: '14', country: 'uk', isActive: true}
       ]
   return (
-    <div>
+    <div className='table'>
          {users.map((user) => (
         <div>
-            <h1>{user.name}</h1>
-            <p>{user.age}</p>
-            <p>{user.location}</p>
+            <h1>{user.userName}</h1>
+            <p>{user.name}</p>
+            <p>{user.posts}</p>
+            <p>{user.comments}</p>
+            <p>{user.country}</p>
+            {user?.isActive && <h1>{user.name} is active</h1>}
         </div>
-
 ))} 
     </div>
   )
