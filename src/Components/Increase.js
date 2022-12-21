1619369231cb73fd8b8db03dde12071ba7c90823
mohamud <mohmud141@gmail.com>
2022-12-21 21:06:57 +0300

    added an edition

--- src/Components/Increase.js
+++ src/Components/Increase.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
+import { Prev } from 'react-bootstrap/esm/PageItem'
 
 function Increase() {
     const [number, setNumber] = useState(0)
+    const [name, setName] = useState('mohamud')
+
 
     const IncreaseNum = () =>{
-        setNumber(number + 1)
+        setNumber((PrevNumber) => PrevNumber + 1)
     }
-    const increaseAsy = () =>{
+    const IncreaseAsy = () =>{
         setTimeout(() =>{
             setNumber((PrevNumber) => PrevNumber + 1)
         },2000)
     }
+    
   return (
     <div>
         <h1>{number}</h1>
+        <h1>{name}</h1>
         <button onClick={IncreaseNum}>IncreaseNum</button>
-        <button onClick={increaseAsy}>increaseAsy</button>
+        <button onClick={IncreaseAsy}>IncreaseAsy</button>
+        <button onClick={() =>setName('linconl')}>names</button>
     </div>
   )
 }
