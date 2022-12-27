a25ae3c0ad928234e5d0613a3685d3d609dd7feb
mohamud <mohmud141@gmail.com>
2022-12-27 21:18:10 +0300

    props in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import Increase from './Components/Increase';
 import Learning from './Components/Learning';
+import Products from './Components/Products';
 import TableOne from './Components/TableOne';
 import Timer from './Components/Timer';
 import StudyUseEff from './StudyUseEff';
 function App() {
   return (
     <div className="App">
-      <Timer />
+        <h1>Products</h1>
+      <div>
+      <Products
+          img="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/82/6142201/1.jpg?2933"
+          name="Cyxus"
+          desc="Non-Slip Fitness Leisure Running Sneakers"
+          price="$29"
+        />
+        <Products
+          img="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/01/241417/1.jpg?6747"
+          name="Vitike"
+          desc="Latest Men Sneakers -Black"
+          price="$100"
+        />
+        <Products
+          img="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/06/4410121/1.jpg?4437"
+          name="Aomei"
+          desc="Men's Trend Casual Sports Shoe"
+          price="$40"
+        />
+      </div>
+     
+      {/* <Timer /> */}
       {/* <StudyUseEff/> */}
       {/* <Increase/> */}
       {/* <Learning/> */}
